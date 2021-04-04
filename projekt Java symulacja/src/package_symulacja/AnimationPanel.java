35b99c764f9513a0628fd858175036bb4d985131
olala99 <olacichon555@gmail.com>
2021-04-04 19:58:30 +0200

    changed layouts/placements of components etc

--- projekt Java symulacja/src/package_symulacja/AnimationPanel.java
+++ projekt Java symulacja/src/package_symulacja/AnimationPanel.java
@@ -1,22 +1,43 @@
+/* panel centralny:
+ * panel z tlem do animacji
+ * panel z przyciskami kontrolujacymi AnimationControlPanel */
+
 package package_symulacja;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.*;
 
 public class AnimationPanel extends JPanel {
 
 	private JPanel animationPanel;
 	private AnimationControlPanel animationControlPanel;
+	private JPanel centerPanel;
 	
 	public AnimationPanel() {
 		this.setLayout(new BorderLayout());
+		this.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
 		
+		centerPanel = new JPanel();
 		animationPanel = new JPanel();
 		animationControlPanel = new AnimationControlPanel();
-		
 		animationPanel.setBackground(Color.white);
 		
-		this.add(animationPanel,BorderLayout.CENTER);
-		this.add(animationControlPanel,BorderLayout.SOUTH);
+		animationControlPanel.buttonBGColor.addActionListener(new ActionListener(){
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				Color backgroundColor = JColorChooser.showDialog(getParent(), "Wybierz kolor", Color.white);
+				animationPanel.setBackground(backgroundColor);	
+			}
+			
+		});
+		
+		centerPanel.setLayout(new BorderLayout());
+		centerPanel.add(animationPanel,BorderLayout.CENTER);
+		centerPanel.add(animationControlPanel, BorderLayout.SOUTH);
+		this.add(centerPanel,BorderLayout.CENTER);
 	}
 }
