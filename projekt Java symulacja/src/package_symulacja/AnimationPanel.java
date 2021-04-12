5701ad8969453bff7044a0e28c208e775c735754
olala99 <79869208+olala99@users.noreply.github.com>
2021-04-12 21:46:48 +0200

    Update AnimationPanel.java
    dodano absorbent

--- projekt Java symulacja/src/package_symulacja/AnimationPanel.java
+++ projekt Java symulacja/src/package_symulacja/AnimationPanel.java
@@ -1,24 +1,26 @@
 /* autorka: Ola
  * panel centralny:
- * panel z tlem do animacji
+ * panel z tlem do animacji + absorbent
  * panel z przyciskami kontrolujacymi AnimationControlPanel */
 
 package package_symulacja;
 
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
 import javax.swing.*;
 
 public class AnimationPanel extends JPanel {
 
 	JPanel animationPanel;
+	Absorbent absorbent;
 
 	public AnimationPanel() {
 		this.setLayout(new BorderLayout());
 		animationPanel = new JPanel();
+		animationPanel.setLayout(new BorderLayout());
 		animationPanel.setBackground(Color.white);
+		
+		absorbent = new Absorbent();
+		animationPanel.add(absorbent);
 
 		this.add(animationPanel,BorderLayout.CENTER);
 	}
