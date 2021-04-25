5bef9468e0eae5ea08bdcfc0d5cfe2f3474f0647
olala99 <79869208+olala99@users.noreply.github.com>
2021-04-25 15:53:29 +0200

    dodano fun. setVisible, getVisible, removeParticle

--- projekt Java symulacja/src/package_symulacja/EnergyParticle.java
+++ projekt Java symulacja/src/package_symulacja/EnergyParticle.java
@@ -12,11 +12,11 @@ public class EnergyParticle{
 
 	  private int xPos = 1; // x position
 	  private int yPos = 1; // y posotion
-	  private int vX = rand.nextInt(100);//-5; // x component velocity
+	  private int vX = 6 + rand.nextInt(6);//-5; // x component velocity
 	  private int vY = rand.nextInt(100);//-5; // y component velocity
 	  private int d = 5; // diameter of the particle
 	  private Color color = Color.BLACK; // particle colour
-    
+      private boolean visibleParticle = true; 
    
     public void setVX(int vx) {
 		this.vX = vx;
@@ -60,12 +60,23 @@ public class EnergyParticle{
 		return color;
   	}
 
-	  public void setColor(Color color) {
-		this.color = color;
-	  }
-	  
-
-
+	 public void setColor(Color color) {
+		 this.color = color;
+	 }
+	 
+	 public void setVisible(boolean v) {
+		 this.visibleParticle = v;
+	 }
+	 
+	 public boolean getVisible() {
+		 return visibleParticle; 
+	 }
+	 
+	 public void removeParticle(Color c) {
+		 this.setColor(c);
+		 
+	 }
+	
 	  public void paint(Graphics g){
         g.setColor(getColor());
         g.fillOval(xPos, yPos, d, d);
