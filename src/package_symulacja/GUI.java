192627ec9133c584ca35fd102c84f36fc6036ccd
AlinaParada <79859222+AlinaParada@users.noreply.github.com>
2021-06-15 21:30:38 +0200

    Add files via upload

---
+++ src/package_symulacja/GUI.java
@@ -0,0 +1,18 @@
+/* autorka: Alina */
+package package_symulacja;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+public class GUI {
+	
+	public GUI() {
+	}
+	
+	public static void main(String[] args) 
+	{	
+		Frame frame = new Frame();
+		frame.setVisible(true);
+	}
+}
+
